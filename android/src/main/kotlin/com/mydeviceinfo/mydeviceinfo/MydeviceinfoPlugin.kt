fe0127e6e85dde3ce835ded05c96decea2fc3832
Viktor Kot <vkotyk94@gmail.com>
2022-11-16 20:34:13 +0100

    add native code for device info

--- android/src/main/kotlin/com/mydeviceinfo/mydeviceinfo/MydeviceinfoPlugin.kt
+++ android/src/main/kotlin/com/mydeviceinfo/mydeviceinfo/MydeviceinfoPlugin.kt
@@ -1,6 +1,7 @@
 package com.mydeviceinfo.mydeviceinfo
 
 import androidx.annotation.NonNull
+import android.os.Build
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
@@ -17,13 +18,37 @@ class MydeviceinfoPlugin: FlutterPlugin, MethodCallHandler {
   private lateinit var channel : MethodChannel
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "mydeviceinfo")
+    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "com/mydeviceinfo")
     channel.setMethodCallHandler(this)
   }
 
+
+  private fun getSystemDetail(): String {
+    return "Brand: ${Build.BRAND} \n" +
+            "DeviceID: ${
+                Settings.Secure.getString(
+                    contentResolver,
+                    Settings.Secure.ANDROID_ID
+                )
+            } \n" +
+            "Model: ${Build.MODEL} \n" +
+            "ID: ${Build.ID} \n" +
+            "SDK: ${Build.VERSION.SDK_INT} \n" +
+            "Manufacture: ${Build.MANUFACTURER} \n" +
+            "Brand: ${Build.BRAND} \n" +
+            "User: ${Build.USER} \n" +
+            "Type: ${Build.TYPE} \n" +
+            "Base: ${Build.VERSION_CODES.BASE} \n" +
+            "Incremental: ${Build.VERSION.INCREMENTAL} \n" +
+            "Board: ${Build.BOARD} \n" +
+            "Host: ${Build.HOST} \n" +
+            "FingerPrint: ${Build.FINGERPRINT} \n" +
+            "Version Code: ${Build.VERSION.RELEASE}"
+  }
+
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "mydeviceinfo") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
+    if (call.method == "myDeviceInfo") {
+      result.success(getSystemDetail())
     } else {
       result.notImplemented()
     }
