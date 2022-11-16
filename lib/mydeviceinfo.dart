fb8bb3335b60fa27c2253fdf2de94234a9e92767
Viktor Kot <vkotyk94@gmail.com>
2022-11-16 20:32:47 +0100

    Create method for obtaining infromation about device

--- lib/mydeviceinfo.dart
+++ lib/mydeviceinfo.dart
@@ -1,8 +1,21 @@
+import 'package:flutter/services.dart';
 
 import 'mydeviceinfo_platform_interface.dart';
 
 class Mydeviceinfo {
+  static const MethodChannel _channel = MethodChannel('com/mydeviceinfo');
+
   Future<String?> getPlatformVersion() {
     return MydeviceinfoPlatform.instance.getPlatformVersion();
+  }
+
+  Future<String?> getMyDeviceInfo() async {
+    try {
+      final myDeviceInfo = await _channel.invokeMethod<String>('myDeviceInfo');
+
+      return myDeviceInfo;
+    } on PlatformException {
+      return null;
+    }
   }
 }
